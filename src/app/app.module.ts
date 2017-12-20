38d684d3f2eba0a9e05abc8b917d95c31fc2eb5c
Abdul <abdul.basit@sayahconsulting.com>
2017-12-20 17:15:26 +0500

    Simple User Demo App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { UsersComponent } from './users/users.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { UserComponent } from './users/user-list/user/user.component';
+import { DropdownDirective } from './shared/dropdown.directive';
+import { UserService } from './users/user.service';
+import { HomeComponent } from './home/home.component';
+import { EditUserComponent } from './users/edit-user/edit-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UsersComponent,
+    UserListComponent,
+    UserComponent,
+    DropdownDirective,
+    HomeComponent,
+    EditUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
